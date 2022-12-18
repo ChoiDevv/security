7f703c9b060c3899e8e85342db012d4f5e4f51c4
ChoiDevv <oinkoink71@naver.com>
2022-12-18 17:17:50 +0900

    add: security

--- src/main/java/com/example/security/SecurityController.java
+++ src/main/java/com/example/security/SecurityController.java
@@ -1,4 +1,13 @@
 package com.example.security;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
 }
